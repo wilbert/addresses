07e6888149c76df220c2cd119946507af7683333
Altherlex Alves <para.alves@gmail.com>
2016-07-29 18:20:57 -0300

    autocomplete field

--- app/controllers/addresses/cities_controller.rb
+++ app/controllers/addresses/cities_controller.rb
@@ -1,15 +1,12 @@
-require_dependency "addresses/application_controller"
-
 module Addresses
   class CitiesController < ApplicationController
-    respond_to :json
+    respond_to :html
+    autocomplete :city, :name, display_value: :city_and_state, full_model:true, class_name:'Addresses::City', case_sensitive:true, extra_data:['state_id', 'state_name'] 
 
     def index
-        @state = State.find(params[:state_id])
-        
-        @cities = @state.cities.order("name asc")
-
-        respond_with @cities
+      @state = State.find(params[:state_id])
+      @cities = @state.cities.order(:relevance, :name)
+      respond_with @cities
     end
   end
 end
--- app/models/addresses/city.rb
+++ app/models/addresses/city.rb
@@ -1,7 +1,24 @@
 module Addresses
   class City < ActiveRecord::Base
     belongs_to :state
-
     has_many :neighborhoods
+
+    # scope :autocomplete, -> { joins(:state) }
+
+    def city_and_state
+      "#{name}/#{state_acronym}"
+    end
+
+    def state_id
+      state.id
+    end
+
+    def state_name
+      state.name
+    end
+    
+    def state_acronym
+      state.acronym
+    end
   end
 end
