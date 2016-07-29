cbe93cbadd32616249a047b8dc482bcbad796bf4
Altherlex Alves <para.alves@gmail.com>
2016-07-29 18:22:21 -0300

    refatoring

--- lib/addresses/engine.rb
+++ lib/addresses/engine.rb
@@ -1,12 +1,20 @@
+# require 'rails-jquery-autocomplete'
+require 'rails4-autocomplete'
+require 'jquery-ui-rails'
+
 module Addresses
   class Engine < ::Rails::Engine
     isolate_namespace Addresses
 
+    # initializer "addresses.assets.precompile" do |app|
+    #   app.config.assets.precompile += %w(application.js)
+    # end    
+
     config.generators do |g|
-        g.test_framework      :rspec,        :fixture => false
-        g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-        g.assets false
-        g.helper false
+      g.test_framework      :rspec,        :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
     end
   end
 end
