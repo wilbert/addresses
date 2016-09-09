37f9634b4c8ed660e04b2bd8dc93d6ac35ae40ba
Joice Taciana <joicetaciana@gmail.com>
2016-09-09 18:05:52 -0300

    Apply new rspec sintax on neighborhoods specs

--- spec/requests/neighborhoods_spec.rb
+++ spec/requests/neighborhoods_spec.rb
@@ -1,21 +1,18 @@
 require 'spec_helper'
 
-describe Addresses::NeighborhoodsController do
-    #routes { Addresses::Engine.routes }
-    
-    before (:each) do
-        @state = FactoryGirl.create(:addresses_state)
-        @city = FactoryGirl.create(:addresses_city, state: @state)
-        @neighborhood = FactoryGirl.create(:addresses_neighborhood, city: @city)
-    end
+RSpec.describe Addresses::NeighborhoodsController, type: :request do
+  let!(:state) { create :state }
+  let!(:city) { create :city, state: state }
+  let!(:neighborhood) { create :neighborhood, city: city }
 
-    describe "GET /neighborhoods" do 
-        it "should return an interview" do
-            get addresses.neighborhoods_path(city_id: @city.id), format: "json"
-            
-            json = JSON.parse(response.body)
+  describe "GET /neighborhoods" do
+    before { get '/addresses/neighborhoods', params: { city_id: city.id, format: "json" } }
 
-            json[0]["name"].should == @neighborhood.name
-        end
+    it "should return an interview" do
+      json = JSON.parse(response.body)
+
+      expect(response.status).to be(200)
+      expect(json[0]["name"]).to eq(neighborhood.name)
     end
-end                         
+  end
+end
