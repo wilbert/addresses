9f22e261be6bb404a78b178560b51a3d281378c8
Joice Taciana <joicetaciana@gmail.com>
2016-09-09 18:06:05 -0300

    Add missing specs to models

--- spec/models/addresses/address_spec.rb
+++ spec/models/addresses/address_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
-module Addresses
-  describe Address do
-    pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Addresses::Address, type: :model do
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:zipcode) }
+    it { is_expected.to validate_presence_of(:number) }
+    it { is_expected.to validate_presence_of(:city_id) }
+    it { is_expected.to validate_presence_of(:state_id) }
+  end
+
+  describe 'association' do
+    it { is_expected.to belong_to(:city) }
+    it { is_expected.to belong_to(:neighborhood) }
+    it { is_expected.to belong_to(:addressable) }
+  end
+
+  describe '#to_s' do
+    let!(:address) { create :address }
+    it { expect(address.to_s).to eq("Street name, Number, Neighborhood name. City name - State acronym") }
   end
 end
