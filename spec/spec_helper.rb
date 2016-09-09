37fb35365f91aa9e3e5957a25dcd1f4630aeae1c
Joice Taciana <joicetaciana@gmail.com>
2016-09-09 17:59:53 -0300

    Prepare environment test to use gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,19 +9,21 @@ end
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'factory_girl_rails'
+require 'shoulda/matchers'
 
 Rails.backtrace_cleaner.remove_silencers!
-
-FactoryGirl.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
-FactoryGirl.find_definitions
-
 # Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f; }
+
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
+  config.include FactoryGirl::Syntax::Methods
   config.order = "random"
+
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
