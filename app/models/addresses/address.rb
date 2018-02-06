e26e463e21822b6be9e5883b1b227ce13465c8a6
Wilbert Ribeiro <wkelyson@gmail.com>
2018-02-06 17:44:57 -0300

    Add conditional validation to zipcode_id.

--- app/models/addresses/address.rb
+++ app/models/addresses/address.rb
@@ -3,7 +3,8 @@ module Addresses
     belongs_to :zipcode
     belongs_to :addressable, polymorphic: true
 
-    validates :zipcode_id, :number, presence: true
+    validates :number, presence: true
+    validates :zipcode_id, presence: true, if: Proc.new{|a| a.zipcode.street.blank? }
     delegate :street, :neighborhood, :city, to: :zipcode
 
     accepts_nested_attributes_for :zipcode, allow_destroy: false
