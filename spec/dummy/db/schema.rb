7e8e94d4c3b4d5666e35d440376c4717685ccc7c
Wilbert Ribeiro <wkelyson@gmail.com>
2019-06-17 21:01:24 -0300

    fixing new way to run rake tasks

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,59 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920174406) do
+ActiveRecord::Schema.define(version: 2019_06_14_190964) do
 
   create_table "addresses_addresses", force: :cascade do |t|
-    t.string   "number"
-    t.string   "complement"
-    t.integer  "addressable_id"
-    t.string   "addressable_type"
+    t.string "number"
+    t.string "complement"
+    t.integer "addressable_id"
+    t.string "addressable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "zipcode_id"
+    t.integer "zipcode_id"
     t.index ["addressable_id"], name: "index_addresses_addresses_on_addressable_id"
     t.index ["zipcode_id"], name: "index_addresses_addresses_on_zipcode_id"
   end
 
   create_table "addresses_cities", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "state_id"
+    t.string "name"
+    t.integer "state_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["state_id"], name: "index_addresses_cities_on_state_id"
   end
 
   create_table "addresses_countries", force: :cascade do |t|
-    t.string   "name"
-    t.string   "acronym"
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "addresses_neighborhoods", force: :cascade do |t|
-    t.integer  "city_id"
-    t.string   "name"
+    t.integer "city_id"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["city_id"], name: "index_addresses_neighborhoods_on_city_id"
   end
 
   create_table "addresses_states", force: :cascade do |t|
-    t.string   "name"
-    t.string   "acronym"
-    t.integer  "country_id"
+    t.string "name"
+    t.string "acronym"
+    t.integer "country_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["country_id"], name: "index_addresses_states_on_country_id"
   end
 
   create_table "addresses_zipcodes", force: :cascade do |t|
-    t.string   "street"
-    t.integer  "city_id"
-    t.integer  "neighborhood_id"
-    t.string   "number"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "street"
+    t.integer "city_id"
+    t.integer "neighborhood_id"
+    t.string "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_addresses_zipcodes_on_city_id"
     t.index ["neighborhood_id"], name: "index_addresses_zipcodes_on_neighborhood_id"
   end
