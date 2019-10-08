4d6f150889124e26ba4a525a835113605c74d5a9
Wilbert Ribeiro <wkelyson@gmail.com>
2019-10-08 02:39:07 -0300

    Fix table names

--- db/migrate/20191008052817_add_indexes.rb
+++ db/migrate/20191008052817_add_indexes.rb
@@ -2,20 +2,20 @@
 
 class AddIndexes < ActiveRecord::Migration[6.0]
   def change
-    add_index :neighborhoods, :name
-    add_index :neighborhoods, [:name, :city_id]
+    add_index :addresses_neighborhoods, :name
+    add_index :addresses_neighborhoods, [:name, :city_id]
 
-    add_index :countries, :name
-    add_index :countries, :acronym
+    add_index :addresses_countries, :name
+    add_index :addresses_countries, :acronym
 
-    add_index :cities, :name
-    add_index :cities, [:name, :state_id]
+    add_index :addresses_cities, :name
+    add_index :addresses_cities, [:name, :state_id]
 
-    add_index :states, :name
-    add_index :state, [:name, :country_id]
+    add_index :addresses_states, :name
+    add_index :addresses_states, [:name, :country_id]
 
-    add_index :zipcodes, :number
+    add_index :addresses_zipcodes, :number
 
-    add_index :addresses, [:addressable_id, :addressable_type]
+    add_index :addresses_addresses, [:addressable_id, :addressable_type]
   end
 end
