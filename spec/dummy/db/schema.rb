31c6a33f318159de12d125566e33cdc327aa5cc6
Wilbert Ribeiro <wkelyson@gmail.com>
2019-10-10 21:38:56 -0300

    Bump version

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_235720) do
+ActiveRecord::Schema.define(version: 2019_10_11_003840) do
 
   create_table "addresses_addresses", force: :cascade do |t|
     t.string "number"
