85a4473a3f8301006afc873ef4f33c2e39970767
Wilbert Ribeiro <wkelyson@gmail.com>
2023-11-28 15:49:28 -0300

    disable email

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -15,6 +15,7 @@ Dummy::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -25,5 +26,5 @@ Dummy::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  # config.assets.debug = true
 end
