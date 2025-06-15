b318fd2d390ebccddf2e9c44d2caefc4b83be78e
Wilbert Ribeiro <wkelyson@gmail.com>
2025-06-15 09:00:14 -0300

    Update zipcodes_controller.rb

--- app/controllers/addresses/zipcodes_controller.rb
+++ app/controllers/addresses/zipcodes_controller.rb
@@ -8,7 +8,23 @@ module Addresses
       @zipcode = Zipcode.find_by(number: params[:zipcode])
 
       if @zipcode.present?
-        render json: @zipcode
+        city = @zipcode.city
+        state = city.state
+        neighborhood = @zipcode.neighborhood
+
+        states = State.all.order(:name).map { |s| { id: s.id, name: s.name, acronym: s.acronym } }
+        cities = state.cities.order(:name).map { |c| { id: c.id, name: c.name } }
+        neighborhoods = city.neighborhoods.order(:name).map { |n| { id: n.id, name: n.name } }
+
+        render json: {
+          zipcode: @zipcode,
+          states: states,
+          selected_state_id: state.id,
+          cities: cities,
+          selected_city_id: city.id,
+          neighborhoods: neighborhoods,
+          selected_neighborhood_id: neighborhood&.id
+        }
       else
         render json: { errors: :zipcode_not_found }, success: false, status: :unprocessable_entity
       end
